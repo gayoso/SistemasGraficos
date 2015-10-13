774bc82a90f2053705c9beaf2cfb946bf4223534
Gabriel Gayoso <ga-yo-so@hotmail.com>
2015-10-13 03:44:09 -0300

    agrego primera implementacion (ni la probe asi que seguro tira erroes) de una clase para componer los arboles de objetos

---
+++ conjunto.js
@@ -0,0 +1,58 @@
+var Conjunto = function() {
+	this.matrix_local = mat4.create();
+	this.matrix_total = mat4.create();
+	/*this.position = vec3.fromValues(0, 0, 0);
+	this.scale = vec3.fromValues(1, 1, 1);
+	this.rotation = vec3.fromValues(0, 0, 0);*/
+	this.parent = null;
+	this.children = [];
+}
+
+Conjunto.prototype = {
+
+	constructor = Conjunto,
+	
+	// modifica los vertices segun una matriz de escala+rotacion+traslacion
+	applyMatrix = function(m){
+		// ver si esto esta bien, o la mult es al reves
+		this.matrix.multiply(this.matrix, this.matrix, m);
+	},
+	
+	// vuelve al estado inicial todos los vertices, aplica la transf y propaga los cambios a sus hijos
+	setTransform = function(m){
+		/*this.position = vec3.fromValues(0, 0, 0);
+		this.scale = vec3.fromValues(1, 1, 1);
+		this.rotation = vec3.fromValues(0, 0, 0);*/
+		this.matrix.identity(this.matrix);
+		this.applyMatrix(m);
+		this.updateMatrix();
+	},
+	
+	updateMatrix = function(){
+		if(this.parent === null){
+			this.matrix_total.copy(this.matrix);
+		} else {
+			// ver si esto esta bien, o la mult es al reves
+			this.matrix_total.multiply(this.matrix_total, this.parent.matrix_total, this.matrix_total);
+		}
+		
+		for ( var i = 0, l = this.children.length; i < l; i ++ ) {
+			this.children[i].updateMatrix();
+		}
+	},
+	
+	// agrega un hijo (y me agrego como padre en el hijo)
+	add = function(object){
+		object.parent = this;
+		this.children.push(object);
+	}
+	
+	// borra a un hijo (y se borra como padre en el hijo)
+	remove = function(object){
+		var index = this.children.indexOf(object);
+		if(index !== -1){
+			object.parent = null;
+			this.children.splice(index, 1);
+		}
+	}
+}
