a8556c95327dab33f132ac1928b4dcfd9d0922cb
Nicolás Keklikian <nkeklikian@gmail.com>
2015-10-22 20:37:53 -0300

    le puse puntos un poco mas parecidos a los de la foto

--- montaniarusa.js
+++ montaniarusa.js
@@ -6,33 +6,16 @@ var MontaniaRusa = function(){
 	// es un posible recorrido de la montaña rusa, puede ser otro da lo mismo
 	
 	var puntos = [];
-	puntos.push(vec3.fromValues(5, 2, 0));
-	puntos.push(vec3.fromValues(5, 2, 4));
-	puntos.push(vec3.fromValues(5, 3, 5.5));
-	puntos.push(vec3.fromValues(5, 4, 7));
-	// giros horizontales
-	puntos.push(vec3.fromValues(5, 6, 8));
-	puntos.push(vec3.fromValues(7, 6.5, 10));
-	puntos.push(vec3.fromValues(8, 6.25, 9.5));
-	puntos.push(vec3.fromValues(8.5, 6, 9));
-	puntos.push(vec3.fromValues(8, 6, 8.5));
-	puntos.push(vec3.fromValues(7.5, 6, 9));
-	puntos.push(vec3.fromValues(8, 5.75, 9.5));
-	puntos.push(vec3.fromValues(8.5, 5.5, 9));
-	puntos.push(vec3.fromValues(8, 5.25, 8.5));
-	puntos.push(vec3.fromValues(7.5, 5.25, 9));
-	puntos.push(vec3.fromValues(8, 5.25, 9.5));
-	puntos.push(vec3.fromValues(8.5, 5, 9));
-	// end
-	puntos.push(vec3.fromValues(8.5, 4.5, 8));
-	puntos.push(vec3.fromValues(8.75, 3, 5));
-	puntos.push(vec3.fromValues(9, 5, 3));
-	puntos.push(vec3.fromValues(8.8, 7, 4));
-	puntos.push(vec3.fromValues(8.6, 5, 5));
-	puntos.push(vec3.fromValues(8.5, 3, 4));
-	puntos.push(vec3.fromValues(8.3, 3, 2));
-	puntos.push(vec3.fromValues(8, 2.5, 0.5));
-	puntos.push(vec3.fromValues(5, 2, 0));
+	puntos.push(vec3.fromValues(0, 0, 0));
+	puntos.push(vec3.fromValues(4, 0, 0));
+	puntos.push(vec3.fromValues(4, 0, 4));
+	puntos.push(vec3.fromValues(0, 2, 4));
+	puntos.push(vec3.fromValues(0, 4, 0));
+	puntos.push(vec3.fromValues(0, 6, -4));
+	puntos.push(vec3.fromValues(-4, 4, -4));
+	puntos.push(vec3.fromValues(-4, 2, -2));
+	puntos.push(vec3.fromValues(-4, 0, 0));
+	puntos.push(vec3.fromValues(0, 0, 0));
 	
 	/*** DESCOMENTAR ESTO PARA CREAR UNA CURVA ***/
 	
