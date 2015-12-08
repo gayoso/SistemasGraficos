09aca70f04e8a6057f32ee16e1d55f7c8dd28141
Nicolás Keklikian <nkeklikian@gmail.com>
2015-12-08 16:32:29 -0300

    Refactors y boludeces, mejore un poco la montaña rusa

--- montaniarusa.js
+++ montaniarusa.js
@@ -6,7 +6,6 @@ var MontaniaRusa = function(){
 	Conjunto.call(this);
 		/*** ESTOS PUNTOS SE USAN EN LA CREACION DE LAS CURVAS ***/
 	// es un posible recorrido de la montaña rusa, puede ser otro da lo mismo
-	
 
 	this.puntos = [];
 	
@@ -22,10 +21,8 @@ var MontaniaRusa = function(){
 	this.puntos.push(vec3.fromValues(0, 20, -30));
 	this.puntos.push(vec3.fromValues(0, 15, -45));
 	this.puntos.push(vec3.fromValues(-20, 15, -45));
-	this.puntos.push(vec3.fromValues(-20, 10, -20));
-
-	this.puntos.push(vec3.fromValues(-20, 10, -10));
-	this.puntos.push(vec3.fromValues(-20, 0, 0));
+	this.puntos.push(vec3.fromValues(-20, 5, -15));
+	this.puntos.push(vec3.fromValues(-15, 0, 0));
 	this.puntos.push(vec3.fromValues(0, 0, 0));
  
 	/*
@@ -58,7 +55,6 @@ var MontaniaRusa = function(){
 	puntos.push(vec3.fromValues(32, 10, 8));
 	puntos.push(vec3.fromValues(20, 8, 0));*/
 
-
 	var puntosArray = [];
 	
 	puntosArray.push([0, 0, 0]);
@@ -73,9 +69,8 @@ var MontaniaRusa = function(){
 	puntosArray.push([0, 20, -30]);
 	puntosArray.push([0, 15, -45]);
 	puntosArray.push([-20, 15, -45]);
-	puntosArray.push([-20, 10, -20]);
-	puntosArray.push([-20, 10, -10]);
-	puntosArray.push([-20, 0, 0]);
+	puntosArray.push([-20, 5, -15]);
+	puntosArray.push([-15, 0, 0]);
 	puntosArray.push([0, 0, 0]);
 /*
 	puntosArray.push([20, 8, 0]);
@@ -106,16 +101,15 @@ var MontaniaRusa = function(){
 	puntosArray.push([32, 10, 8]);
 	puntosArray.push([20, 8, 0]);*/
 
-
-
-
-
-
+	//Puntos para formar los tres rieles
 	var puntosCentral = [];
 	var puntosExterior = [];
 	var puntosInterior = [];
+	//Tablas que unen los tres rieles
 	this.tablas = new Conjunto();
+	//Columnas que sostienen los rieles
 	this.columnas = new Conjunto();
+	this.rieles = new Conjunto();
 	this.carrito = new Carrito();
 	var curva = new Curva(this.puntos);
 	var derivadas = curva.getDerivada();
@@ -126,55 +120,36 @@ var MontaniaRusa = function(){
 		var puntoBase = puntoDerivado[0];
 		var derivada = puntoDerivado[1];
 		var binormal = vec3.create();
-		
 		vec3.normalize(derivada, derivada);
 		vec3.cross(binormal, derivada, vec3.fromValues(0,1,0));
 		vec3.scale(binormal, binormal, 2);
-		var puntoNuevo = vec3.create();
-		vec3.add(puntoNuevo, puntoBase, binormal);
-		puntosInterior.push(puntoNuevo);
-		var otroPunto = vec3.create();
-		vec3.subtract(otroPunto, puntoBase, binormal);
-		puntosExterior.push(otroPunto);
-		var baseMasDerivada = vec3.create();
-		vec3.normalize(derivada, derivada);
-		vec3.scale(derivada, derivada, 10);
-		var baseMasDerivadaSobreDos = vec3.create();
-		vec3.add(baseMasDerivadaSobreDos, baseMasDerivadaSobreDos, binormal);
-		vec3.scale(baseMasDerivadaSobreDos, baseMasDerivadaSobreDos, 0.5);
-		vec3.add(baseMasDerivadaSobreDos, baseMasDerivadaSobreDos, puntoBase);
-		vec3.add(baseMasDerivada, puntoBase, binormal);
+		//Armo la tabla
+		var puntoInterior = vec3.create();
+		vec3.add(puntoInterior, puntoBase, binormal);
+		puntosInterior.push(puntoInterior);
+		var puntoExterior = vec3.create();
+		vec3.subtract(puntoExterior, puntoBase, binormal);
+		puntosExterior.push(puntoExterior);
 		vec3.add(puntoBase, puntoBase, vec3.fromValues(0, -0.5 ,0));
-		var puntosDerivada = [puntoNuevo, puntoBase, otroPunto];
-		var flechaDerivada = new Mesh(new Curva(puntosDerivada));
+		var puntosTabla = [puntoInterior, puntoBase, puntoExterior];
+		var tablaEntreRieles = new Mesh(new Curva(puntosTabla));
 		var matriz = mat4.create();
 		var c = curva.getCenter();
 		vec3.scale(c, c, -1);
 		mat4.translate(matriz, matriz, c);
-		flechaDerivada.setTransform(matriz);
+		tablaEntreRieles.setTransform(matriz);
 		if(j == 0){
-			this.tablas.add(flechaDerivada);
+			this.tablas.add(tablaEntreRieles);
 		}
-		var baseMasDerivada = vec3.create();
-		vec3.normalize(derivada, derivada);
-		vec3.scale(derivada, derivada, 10);
-		var baseMasDerivadaSobreDos = vec3.create();
-		vec3.subtract(baseMasDerivadaSobreDos, baseMasDerivadaSobreDos, binormal);
-		vec3.scale(baseMasDerivadaSobreDos, baseMasDerivadaSobreDos, 0.5);
-		vec3.add(baseMasDerivadaSobreDos, baseMasDerivadaSobreDos, puntoBase);
-		vec3.subtract(baseMasDerivada, puntoBase, binormal);
-		puntosDerivada = [puntoBase, baseMasDerivadaSobreDos, baseMasDerivada];
-		flechaDerivada = new Mesh(new Curva(puntosDerivada));
-		matriz = mat4.create();
-		mat4.translate(matriz, matriz, c);
-		flechaDerivada.setTransform(matriz);
 		j = (j+1) % 2;
+		//Si estan muy cerca, salteo una tabla
 		if(vec3.distance(ultimoPunto, puntoBase) < 0.2){
 			j = (j+1)%2;
 		}
+		//Pongo columna una de cada diez veces
 		if((i+1)%10 == 0){
 			var puntoPiso = vec3.clone(puntoBase);
-			puntoPiso[1] = -50;
+			puntoPiso[1] = -5;
 			var puntoMedio = vec3.create();
 			vec3.add(puntoMedio, puntoBase, puntoPiso);
 			vec3.scale(puntoMedio, puntoMedio, 0.5);
@@ -184,54 +159,37 @@ var MontaniaRusa = function(){
 			var columna = new Mesh(curvaColumna);
 			columna.setTransform(matriz);
 			this.columnas.add(columna);
-
 		}
-		//this.add(flechaDerivada);
 		ultimoPunto = vec3.clone(puntoBase);
-		
-		
 	}
 	this.add(this.tablas);
 	this.add(this.columnas);
 	this.columnas.setColor(Color.Gray90);
 	
-	this.curvaCentral = new Mesh(new Curva(this.puntos));
-	var m3 = mat4.create();
-	var centro = this.curvaCentral.getCenter();
+	var rielCentral = new Mesh(new Curva(this.puntos));
+	var centro = rielCentral.getCenter();
 	vec3.scale(centro, centro, -1);
-	
-	this.curvas = new Conjunto();
-	
+	var m3 = mat4.create();
 	mat4.translate(m3,m3,centro);
 	this.matTraslacion = mat4.clone(m3);
 	mat4.translate(m3,m3,vec3.fromValues(0,-0.5,0));
-	this.curvaCentral.setTransform(m3);
-	this.curvas.add(this.curvaCentral)
+	rielCentral.setTransform(m3);
+	this.rieles.add(rielCentral)
 	
 	puntosInterior[puntosInterior.length - 1] = puntosInterior[0];
 	var rielInterior = new Curva(puntosInterior);
 	rielInterior.setRadio(0.4);
-	this.curva1 = new Mesh( rielInterior );
-	
-	var m1 = mat4.create();
-	//var centro = this.curva1.getCenter();
-	//vec3.scale(centro, centro, -1);
-	mat4.translate(m1, m1, centro);
-	this.curva1.setTransform(m1);
-	this.curvas.add(this.curva1); 
+	var curvaInterior = new Mesh( rielInterior );
+	curvaInterior.setTransform(this.matTraslacion);
+	this.rieles.add(curvaInterior); 
 
 	puntosExterior[puntosExterior.length - 1] = puntosExterior[0];
 	var rielExterior = new Curva(puntosExterior);
 	rielExterior.setRadio(0.4);
-	this.curva2 = new Mesh(rielExterior);
-	var m2 = mat4.create();
-	//var centro = this.curva2.getCenter();
-	//vec3.scale(centro, centro, -1);
-	mat4.translate(m2, m2, centro);
-	//mat4.translate(m2, m2, vec3.fromValues(-1, 0,0));
-	this.curva2.setTransform(m2);
-	this.curvas.add(this.curva2);
-	this.add(this.curvas);
+	var curvaExterior = new Mesh(rielExterior);
+	curvaExterior.setTransform(this.matTraslacion);
+	this.rieles.add(curvaExterior);
+	this.add(this.rieles);
 	this.ultimoAngulo = 0;
 }
 
@@ -248,6 +206,7 @@ MontaniaRusa.prototype.avanzar = function(){
 	var mat = mat4.create();
 
 	t = this.timer.elapsed_seconds();
+	t = t*1.2;
 	this.curvaCentral = new Mesh(new Curva(this.puntos));
 	var curva = new Curva(this.puntos);
 	var m3 = mat4.create();
@@ -263,7 +222,6 @@ MontaniaRusa.prototype.avanzar = function(){
 	vec3.transformMat4(this.ultimoPuntoCarrito, this.ultimoPuntoCarrito, this.matTraslacion);
 	this.ultimaPendiente = vec3.clone(pendiente);
 	mat4.translate(mat,mat,desplazamiento);
-	
 	mat4.translate(mat, mat, curva.getPunto(t));
 	var x = pendiente[0];
 	var y = pendiente[1];
@@ -283,28 +241,23 @@ MontaniaRusa.prototype.avanzar = function(){
 
 	var adyacente = Math.pow(x*x+z*z,0.5);
 	angulo = -Math.atan((y+0.001)/adyacente);
-	if (y > 0){
-		//angulo = -angulo;
-	} else {
+	if (y < 0){
 		angulo = -angulo;
-	}
+	} 
 	mat4.rotate(mat, mat, angulo , binormal);
 	this.carrito.setTransform(mat);
 	mat = mat4.create();
-
 	mat4.translate(mat, mat, binormal);
 	this.carrito.applyMatrix(mat);
 	mat = mat4.create();
 	mat4.translate(mat, mat, vec3.fromValues(0,2,0));
 	this.carrito.applyMatrix(mat);
 	this.add(this.carrito);
-	//this.add(this.curvaCentral)
-
 }
 MontaniaRusa.prototype.setReflectionTextureName = function(name){
-	ReflectionManager.addReflectionToRenderable(this.curvas, name);
+	ReflectionManager.addReflectionToRenderable(this.rieles, name);
 }
 
 MontaniaRusa.prototype.setReflectiveness = function(ref){
-	this.curvas.setReflectiveness(ref);
+	this.rieles.setReflectiveness(ref);
 }
